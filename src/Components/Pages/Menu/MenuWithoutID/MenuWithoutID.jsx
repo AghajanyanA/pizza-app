f7120cb511b5014ee73c65b387e52508e9f3cd52
AghajanyanA <astara@null.net>
2022-07-17 11:12:09 +0300

    Refactoring, click handling functionality, improvements

--- src/Components/Pages/Menu/MenuWithoutID/MenuWithoutID.jsx
+++ src/Components/Pages/Menu/MenuWithoutID/MenuWithoutID.jsx
@@ -2,30 +2,29 @@ import React, {useEffect, useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {getRestaurants} from "../../../../assets/redux/actions/restaurantsAction";
 import c from '../../../../assets/styles/MenuWithoutID.module.css'
+import {getMenuById} from "../../../../assets/redux/actions/menuAction";
+import Menu from "../Menu"
 
-export const MenuWithoutID = p => {
+export const MenuWithoutID = () => {
     const dispatch = useDispatch()
     useEffect(() => {
         dispatch(getRestaurants())
-    }, [])
+    }, []) //eslint-disable-line
 
     const restaurants = useSelector(state => state.restaurants.data)
-    // console.log(restaurants)
-
-    const [restaurant, setRestaurant] = useState(null)
+    const [active, setActive] = useState(null)
 
     const handleRestSelection = (e) => {
-        console.log(e)
-        e.target.classList.toggle('active')
+        setActive(e.target.attributes.id.value)
+        dispatch(getMenuById(active))
     }
-
-
 
     return (
         <>
-            <div className={c.restaurants}>{restaurants.map(item => <p onClick={handleRestSelection} className={c.resItem} key={item.id}>{item.name}</p>)}</div>
-
-
+            <h3>Hey please choose your desired restaurant below</h3>
+            <div className={c.restaurants} > { restaurants.map(item => <p id={ item.id } onClick={ handleRestSelection }
+                     className={ `${c.resItem} ${active === item.id ? 'active' : ''} `} key={ item.id }>{ item.name }</p>)}</div>
+            {active != null ? <Menu id={active} /> : ''}
             {/*if no id was given then show box to select from available restaurants, else show what's present*/}
             {/*<br/>*/}
             {/*no id, picker component to be here*/}
